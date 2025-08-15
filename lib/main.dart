7205054d474ecb10bcc5286a829ff354a2c819cc
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-08-15 03:20:05 +0300

    add hydrated bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:api_call/manager/cubit/product_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
